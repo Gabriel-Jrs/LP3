1dba5047ae7aa7c42c541dcae9e4777296769e89
𝕲𝖆𝖇𝖗𝖎𝖊𝖑 𝕵𝕽𝕾 <gabriell4924462@gmail.com>
2026-06-15 16:18:45 -0300

    Add files via upload

---
+++ lp3_atv_11/lp3_atv_11/Produto.php
@@ -0,0 +1,69 @@
+<?php
+abstract class Produto
+{
+    private int $codigo;
+    private string $nome;
+    private string $descricao;
+    private float $precoBase;
+    private int $qtdeEstoque;
+    private bool $isAtivo;
+
+    public function getCodigo(): int
+    {
+        return $this->codigo;
+    }
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+    public function getPrecoBase(): float
+    {
+        return $this->precoBase;
+    }
+    public function getTqtdeEstoque(): int
+    {
+        return $this->qtdeEstoque;
+    }
+    public function isAtivo(): bool
+    {
+        return $this->isAtivo;
+    }
+
+    public function adicionarEstoque(int $quantidade)
+    {
+        if ($quantidade > 0) {
+            $this->qtdeEstoque += $quantidade;
+            if ($this->isAtivo == false) {
+                $this->isAtivo == true;
+            } else {
+            }
+        } else {
+            echo 'Invalido';
+        }
+    }
+    public function baixarEstoque(int $quantidade)
+    {
+        $this->qtdeEstoque -= $quantidade;
+        if ($this->qtdeEstoque == 0) {
+            $this->isAtivo = false;
+        }
+    }
+    public function aplicarDesconto($percentual)
+    {
+        if ($percentual < 0.9 && $percentual > 0) {
+            $this->precoBase *= $percentual;
+        } else {
+            echo 'Desconto nao aplicado';
+        }
+    }
+    public abstract function calcularPrecoVenda(){
+
+    }
+    public abstract function verificarDisponibilade(){
+        
+    }
+}
---
+++ lp3_atv_11/lp3_atv_11/ProdutoDigital.php
@@ -0,0 +1,9 @@
+<?php
+    class ProdutoDigital extends Produto{
+        private float $tamanhoArquivoMB;
+        private string $formatoArquivo;
+        private int $limiteDowloadsPermitidos ;
+        private string $linkServidorNuvem;
+
+        
+    }
